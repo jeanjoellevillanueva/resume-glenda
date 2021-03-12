0fd80373555db4949d979c607f6f7fd29dff9649
jeanjoelle <javillanueva@spectrumone.co>
2021-03-13 01:05:36 +0800

    [Task] - Added initial react items

---
+++ src/components/Headers/IndexHeader.js
@@ -0,0 +1,59 @@
+/*!
+
+=========================================================
+* Paper Kit React - v1.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/paper-kit-react
+
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/paper-kit-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+/*eslint-disable*/
+import React from "react";
+
+// reactstrap components
+import { Container } from "reactstrap";
+
+// core components
+
+function IndexHeader() {
+  return (
+    <>
+      <div
+        className="page-header section-dark"
+        style={{
+          backgroundImage:
+            "url(" + require("assets/img/glenda_joelle.png") + ")",
+        }}
+      >
+        <div className="filter" />
+        <div className="content-center">
+          <Container>
+            <div className="title-brand">
+              <h1 className="presentation-title">Increase Amazon Sales in 2021</h1>
+            </div>
+            <h2 className="presentation-subtitle text-center">
+              Scale Your FBA Business With Experienced Amazon Virtual Assistant
+            </h2>
+          </Container>
+        </div>
+        <div
+          className="moving-clouds"
+          style={{
+            backgroundImage: "url(" + require("assets/img/clouds.png") + ")",
+          }}
+        />
+      </div>
+    </>
+  );
+}
+
+export default IndexHeader;
